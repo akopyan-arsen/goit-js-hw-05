eff3e3077dc1cf99adf8213a1d7a1bf8729ab148
akopyan-arsen <tigrikpictures@gmail.com>
2024-02-25 00:53:00 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
 const getTotalBalanceByGender = (users, gender) =>
   users
     .filter(user => user.gender === gender)
-    .reduce((totalBalance, user) => (totalBalance += user.balance), 0);
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 
 const clients = [
   {
